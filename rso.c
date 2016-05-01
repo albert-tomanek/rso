37acf28b22c5df4d1c3da4f16083b8c77d8dccc5
Albert Tomanek <electron826@gmail.com>
2016-05-01 14:15:07 +0100

    c6 - moved code for reading & analysing an RSO file into it's own function.

--- rso.c
+++ rso.c
@@ -24,63 +24,43 @@ uint16_t cvt8to16(uint8_t dataFirst, uint8_t dataSecond)
 	return dataBoth;
 }
 
-int main(int argc, char *argv[])
+struct rsoHeaderData
 {
-	if (! strcmp(argv[1], "-h") || ! strcmp(argv[1], "--help")  || argc < 3)
-	{
-		
-		printf("\
- Syntax: 						\n\
-   ./rso <option> <file>				\n\
-							\n\
- Options: 						\n\
-   -h		= display help.		\n\
-   -a		= Analyse an RSO file (display the header data).		\n\
-   -d		= Dump the sample data to the screen as hexadecimal.	\n\
-	\n\n");
+	int compression;
+	uint16_t length;
+	uint16_t samplerate;
+};
 
-		return 1;
-	}
+int readRsoFile(char *loc, uint8_t *samples, struct rsoHeaderData *header)
+{
+	// Opens the file...
 	
 	printf("Opening file...   ");
 	
 	FILE *inFile;
-	inFile = fopen(argv[2], "r");
+	inFile = fopen(loc, "r");
 	
 	if (! inFile)
 	{
 		printf("error.\n");
-		return 1;
+		exit(1);
 	}
 	else
 	{
 		printf("done.\n");
 	}
 	
-	uint8_t  compression;
+	// Extracts the header data and samples...
+	
+	// Some temporary variables
 	
 	int length1, length2;
-	uint16_t length;
 	
 	int smplrate1, smplrate2;
-	uint16_t smplrate;
 	
 	uint8_t padding1, padding2;		// Just used to check the padding...
 	
-	int divisions; 		// The number of 64kb chunks in the file
-	
-	// And, most importantly,
-	uint8_t samples[65534];
-	
-	/* 65534 is the maximum number of samples in an RSO file,
-	 * since the fiels occupies 2 bytes in the header.
-	 * However, a potential workaround for this could exist
-	 * by putting another header at the end of the sample data,
-	 * thus allowing for another set of 65534 samples.
-	 * this is why samples is a pointer to an array of 65534-byte arrays.
-	 */
-	
-	memset(samples, 0, 65534);
+	//
 	
 	printf("Analysing file...   ");
 	
@@ -92,7 +72,7 @@ int main(int argc, char *argv[])
 		{
 			if (x != 1) {
 				printf("error.\n'> Magic byte at offset 0 should be 01.\n");
-				return 2;
+				exit(2);
 			}
 		}
 		
@@ -100,12 +80,12 @@ int main(int argc, char *argv[])
 		{
 			if (x == 0 || x == 1)
 			{
-				compression = (uint8_t) x;
+				header -> compression =  x;
 			}
 			else
 			{
 				printf("error.\n'> Compression flag at offset 1 should be 00 or 01.\n");
-				return 2;
+				exit(2);
 			}
 		}
 		
@@ -118,7 +98,7 @@ int main(int argc, char *argv[])
 		{
 			length2 = x;
 			
-			length = cvt8to16(length1, length2);
+			header -> length = cvt8to16(length1, length2);
 
 		}
 		
@@ -131,7 +111,7 @@ int main(int argc, char *argv[])
 		{
 			smplrate2 = x;
 			
-			smplrate = cvt8to16(smplrate1, smplrate2);
+			header -> samplerate = cvt8to16(smplrate1, smplrate2);
 
 		}
 		
@@ -147,7 +127,7 @@ int main(int argc, char *argv[])
 			if ( (int) cvt8to16(padding1, padding2) != 0)
 			{
 				printf("error.\n'> Padding at offset 7 & 8 should be 00 00.\n");
-				return 2;
+				exit(2);
 			}
 			
 			printf("done.\n");
@@ -168,26 +148,67 @@ int main(int argc, char *argv[])
 	fclose(inFile);		// Closes the file...
 	printf("done.\n");
 	
-	//divisions = loop / 65534; 	//
 	
+}
+
+int main(int argc, char *argv[])
+{
+	if (! strcmp(argv[1], "-h") || ! strcmp(argv[1], "--help")  || argc < 3)
+	{
+		
+		printf("\
+ Syntax: 				\n\
+   ./rso <option> <file>		\n\
+					\n\
+ Options: 				\n\
+   -h		= display help.		\n\
+   -a		= Analyse an RSO file (display the header data).		\n\
+   -d		= Dump the sample data to the screen as hexadecimal.		\n\
+	\n\n");
+
+		exit(0);
+	}
+	
+
+	// And, most importantly,
+	uint8_t samples[65534];
+	
+	/* 65534 is the maximum number of samples in an RSO file,
+	 * since the fiels occupies 2 bytes in the header.
+	 * However, a potential workaround for this could exist
+	 * by putting another header at the end of the sample data,
+	 * thus allowing for another set of 65534 samples.
+	 * this is why samples is a pointer to an array of 65534-byte arrays.
+	 */
+	
+	memset(samples, 0, 65534);
+	
+	struct rsoHeaderData *rsoHeader = malloc(sizeof(struct rsoHeaderData));
+	
+	
+	// Now for interpreting the cmdline arguments...
 	
 	if (! strcmp(argv[1], "-a") )
 	{
-		if ( (int) compression == 1)
+		readRsoFile(argv[2], &samples[0], rsoHeader);
+		
+		if ( rsoHeader -> compression == 1)
 		{	
 			printf("Compression   = Yes\n");
 		} else {
 			printf("Compression   = No\n");
 		}
-		printf("Sample count  = %d\n", (int) length);		 // "Number of samples  = %04X\n" prints it in HEX.
-		printf("Samplerate    = %dHz", (int) smplrate);
+		printf("Sample count  = %d\n", (int) rsoHeader -> length);		 // "Number of samples  = %04X\n" prints it in HEX.
+		printf("Samplerate    = %dHz", (int) rsoHeader -> samplerate);
 	}
 	
 	if (! strcmp(argv[1], "-d"))
 	{
+		readRsoFile(argv[2], &samples[0], rsoHeader);
+		
 		printf("Dumping chunk 1 as hex:");
 		
-		for (int i = 0; i < (loop-8); i++)
+		for (int i = 0; i < (rsoHeader->length - 8); i++)
 		{
 			if (i % DUMP_WIDTH == 0)  printf("\n   ");
 			
@@ -195,4 +216,5 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	exit(0);
 }
