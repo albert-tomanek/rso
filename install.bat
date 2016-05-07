69c778b5ce5cb9aa158e067a62df0ecb2042b6e2
Albert Tomanek <electron826@gmail.com>
2016-05-07 21:46:49 +0100

    c13 v1 - Release 1; finished README, added win32 binary and installer.

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Tested on Windows XP.
+echo Installing RSO...
+copy bin\rso.exe C:\WINDOWS\system32\
+copy bin\libsndfile-1.dll C:\WINDOWS\system32
+echo RSO has been installed.
+pause
